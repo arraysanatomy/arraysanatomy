dc0b6f66790249bcfe0c849a00d3499201b40c5a
Ramin <ramin_yavari03@yahoo.com>
2016-04-01 14:06:45 -0700

    bug: fixed header sending twice on server

--- server/home/HomeController.js
+++ server/home/HomeController.js
@@ -4,13 +4,12 @@ module.exports = {
 	search: function(req, res) {
 		console.log("req.data: ", req.body);
 		var cafe = req.body ? req.body.cafe.toLowerCase() : null;
-		if (cafe) {
-			var results = db[cafe];
-			if (results) {
-				res.status(200).send(JSON.stringify(results));
-			}
-		} 
-		res.sendStatus(400);
+		if (cafe && db[cafe]) {
+				res.status(200).send(JSON.stringify(db[cafe]));
+		}
+		else{
+			res.sendStatus(400);
+		}
 		// TODO: grab things from database
 	}
 }
