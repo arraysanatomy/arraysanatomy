f732cddec9c8b93066d35108d5c55ddfd6561564
Ramin <ramin_yavari03@yahoo.com>
2016-04-01 11:34:20 -0700

    core: working on passing data between views

--- client/app/landing/landing.js
+++ client/app/landing/landing.js
@@ -1,1 +1,29 @@
 var app = angular.module('mainApp.landing',[]);
+
+app.factory('landingFactory',function($http) {
+
+    var getResults = function(cafeName){
+      return $http({
+        method: 'POST',
+        url: '/results',
+        data: cafeName
+      })
+      .then(function(response) {
+        return response.data;
+      });
+  };
+
+  return {
+    getResults:getResults
+  }
+})
+
+app.controller('landingController',function($scope,
+ $window, $location, landingFactory){
+
+  $scope.getResults = function() {
+
+    landingFactory.getResults()
+  }
+
+})
--- client/app/results/results.js
+++ client/app/results/results.js
@@ -1,1 +1,24 @@
 var app = angular.module('mainApp.results',[]);
+
+app.factory('resultsFactory',function($http){
+
+  var getResults = function(){
+    return $http({
+      method: 'POST',
+      url: '/results'
+    })
+    .then(function(response) {
+      return response.data
+    });
+  };
+
+  return {
+    getResults: getResults
+  }
+})
+
+app.controller('resultsController',function($scope, resultsFactory){
+
+  
+
+})
