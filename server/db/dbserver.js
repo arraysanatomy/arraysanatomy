a3d1371b891422e304ed2ffa25e8cd47ccfdb01d
Maksym <maksymgithub@gmail.com>
2016-04-06 13:26:04 -0700

    Added foreign key to database

--- server/db/dbserver.js
+++ server/db/dbserver.js
@@ -8,28 +8,31 @@ if(!exists) {
 	console.log("Creating DB file.");
 	// fs.openSync('./server/db/' + file, 'w');
 	// fs.openSync(file, 'w');
+	db.serialize(function(){
+		 // db.run("PRAGMA foreign_keys = ON");
+			db.run("CREATE TABLE cafes \
+				(ID INT PRIMARY KEY NOT NULL, \
+				 name VARCHAR(15) NOT NULL, \
+				 address VARCHAR(25) NOT NULL, \
+				 phone VARCHAR(14) NOT NULL \
+				)");
+			
+			db.run("CREATE TABLE menu \
+				(ID INT PRIMARY KEY NOT NULL, \
+					item VARCHAR(15), \
+					rating VARCHAR(5), \
+			    cafeID INTEGER(10), \
+			    FOREIGN KEY(cafeID) REFERENCES cafes(ID) \
+        )");
+		
+		
+		//var stmt = db.prepare("INSERT INTO cafes VALUES (?) ")
 
+	});
 }
 
 
-db.serialize(function(){
-	
-		db.run("CREATE TABLE cafes \
-			(ID INT PRIMARY KEY NOT NULL, \
-			 name VARCHAR(15) NOT NULL, \
-			 address VARCHAR(25) NOT NULL, \
-			 phone VARCHAR(14) NOT NULL \
-			)");
-		
-		db.run("CREATE TABLE menu \
-			(ID INT PRIMARY KEY NOT NULL, \
-				item VARCHAR(15), \
-				rating VARCHAR(5))");
-	
-	
-	//var stmt = db.prepare("INSERT INTO cafes VALUES (?) ")
 
-});
 db.close();
 
 module.exports = db;
