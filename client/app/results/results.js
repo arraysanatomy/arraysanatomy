69743d3f38bbfb3e2f0d735ee747edf1388e939d
Jason Wu <chjasonwu@gmail.com>
2016-04-06 14:39:58 -0700

    core:	add bootstrap ui back to bower.json

--- client/app/results/results.js
+++ client/app/results/results.js
@@ -1,24 +1,13 @@
 var app = angular.module('mainApp.results',['ui.bootstrap']);
 
-// app.factory('resultsFactory',function($http){
-
-
-
-//   return {  };
-// });
-
 app.controller('resultsController',function($scope, $uibModal){
 
   var cafe = localStorage.getItem('servedCafeObject');
   cafe = JSON.parse(cafe);
-  // $scope.cafename = cafe.name;
-  // $scope.rating = cafe.menu.rating;
-  // $scope.menuItem = cafe.menu.name;
+
   $scope.menu = cafe.menu;
-  //"menu":[{"item":"Latte","rating":3},{"item":"White Chocolate Mocha","rating":5},{"item":"Earl Grey Tea","rating":4},{"item":"Flat white","rating":4},{"item":"Mocha Frapucinno","rating":2}]}
-
+  
   $scope.animationsEnabled = true;
-
   $scope.open = function(){
 
     var modalInstance = $uibModal.open({
@@ -36,8 +25,5 @@ app.controller('resultsController',function($scope, $uibModal){
 });
 
 app.controller('modalController',function($scope,$uibModalInstance){
-
-
-
-
+  // TODO implement the modalController logics.
 })
