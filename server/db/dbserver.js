1efe86cfd1ff174f86151835456b1e0218a170e8
Jason Wu <chjasonwu@gmail.com>
2016-04-07 16:25:33 -0700

    core: add mock up database data

--- server/db/dbserver.js
+++ server/db/dbserver.js
@@ -1,34 +1,56 @@
 var sqlite3 = require('sqlite3').verbose();
 var fs = require('fs');
-var file = "./server/db/cafe.db";
+var file = './server/db/cafe.db';
 var db = new sqlite3.Database(file);
 var exists = fs.existsSync(file);
 
-if(!exists) {
-	console.log("Creating DB file.");
-	db.serialize(function(){
-			db.run("CREATE TABLE cafes \
-				(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, \
-				 name VARCHAR(15), \
-				 address VARCHAR(25), \
-				 phone VARCHAR(14) \
-				)");
+var data = require('./db.js');
+var _ = require('underscore');
 
-			db.run("CREATE TABLE menu \
-				(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, \
-					item VARCHAR(15), \
-					rating REAL(1), \
-			    cafeID INTEGER(10), \
-			    FOREIGN KEY(cafeID) REFERENCES cafes(ID) \
-        )");
+
+
+if(!exists) {
+	console.log('Creating DB file.');
+
+  function populateMockUp() {
+    _.each(data, function(cafe){
+      addCafe(cafe.name.toLowerCase());
+      cafe.menu.forEach(function(elem){
+        var _item = {
+          name: cafe.name.toLowerCase(),
+          menuItem: {
+            name: elem.item,
+            rating: elem.rating
+          }
+        };
+        addCafeMenuItem(_item);
+      });
+    });
+  }
+
+	db.serialize(function(){
+		db.run('CREATE TABLE cafes \
+			(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, \
+			 name VARCHAR(15), \
+			 address VARCHAR(25), \
+			 phone VARCHAR(14) \
+			)');
+
+		db.run('CREATE TABLE menu \
+			(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, \
+				item VARCHAR(15), \
+				rating REAL(1), \
+		    cafeID INTEGER(10), \
+		    FOREIGN KEY(cafeID) REFERENCES cafes(ID) \
+      )');
+
+    populateMockUp();
 	});
 }
-
 
 function DBQuery(menuItemObj){
 	var name = menuItemObj.name;
 	db.get("SELECT ID FROM cafes WHERE name = ?", [sqliteEscape(name)], function(err, row){});
-}
 
 // takes a string
 function addCafe(cafeName, cb){
@@ -65,7 +87,6 @@ function doesCafeExist(cafeName, cb){
 };
 
 function doesCafeMenuItemExist(menuItemObj, cb){
-
 	db.get("SELECT ID FROM cafes WHERE name = ?", [sqliteEscape(menuItemObj.name)], function(err, row){
 		  db.get("SELECT item FROM menu WHERE item = ? AND cafeID = ?", [menuItemObj.menuItem.name, row.ID], function(err, row){
 		  	if(cb){
@@ -80,7 +101,7 @@ function getCafe(cafeName, cb){
     var cafe = row;
 		var menu = [];
 
-		db.each("SELECT * FROM menu WHERE cafeID = ?", [cafe.ID], function(err, row){
+		db.each('SELECT * FROM menu WHERE cafeID = ?', [cafe.ID], function(err, row){
 			menu.push(row);
 		}, function(err, numberOfRetrievedRows){
 			var cafeObj = {};
@@ -90,24 +111,6 @@ function getCafe(cafeName, cb){
 		});
 	});
 };
-
-	// TODO: add this to mocha/chai
-  // testing that functions work - later for mocha/chai
-  // var testObj = {
-  // 	name: "testingThis",
-  // 	menuItem: {
-  // 		name: "coffee",
-  // 		rating: 5
-  // 	}
-  // };
-  // db.addCafe("testingThis", function(){
-  // 	db.addCafeMenuItem(testObj, function(){
-  // 		db.getCafe("testingThis", function(row){
-  // 			console.log("row", row);
-  // 		});
-  // 	});
-  // });
-
 
 module.exports = {
 	db: db,
