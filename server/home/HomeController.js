9a10fac296c2488f1d83832650dbd9a5e6eada6f
komali2 <rogersjcaleb@gmail.com>
2016-04-07 10:09:45 -0700

    feat: server can now RESTfully addcafe from DB

--- server/home/HomeController.js
+++ server/home/HomeController.js
@@ -3,7 +3,7 @@ var db = require('../db/dbserver.js');
 module.exports = {
 	search: function(req, res) {
 		var cafe = req.body ? req.body.cafe.toLowerCase() : null;
-    db.doesCafeExist(cafe, function(booler)){
+    db.doesCafeExist(cafe, function(booler){
       if(booler){
         db.getCafe(cafe, function(cafeObj){
           res.status(200).send(JSON.stringify(cafeObj));
@@ -12,12 +12,17 @@ module.exports = {
       else{
         res.status(400).send("That cafe was not found.");
       }
-    }
+    });
 	},
 
   //used on POST to api/items/add
   addMenuItem: function(req, res){
     var menuItem = req.body ? req.body.menuItem.toLowerCase() : null;
+
+
+
+
+
     //TODO: how to determine which cafe to add new menu item to
     // if(dbquery(menuItem.name)){
     //   dbadd(menuItem);
@@ -30,18 +35,18 @@ module.exports = {
   },
 
   addCafe: function(req, res){
-    var newCafe = req.body ? req.body.cafe.toLowerCase() : null;
-    // if(newCafe){
-    //   //check that it doesn't already exist
-    //     if(dbDoesExist(newCafe.name)){
-    //       //if it does
-    //       res.status(400).send('this cafe already exists');
-    //     }
-    //     else{
-    //       dbAddCafe(newCafe);
-    //       res.status(200).send(dbGetCafe(newCafe.name));
-    //     }
-    // }
-    //TODO: handle adding this to a real DB
+    var cafe = req.body ? req.body.cafe.toLowerCase() : null;
+    db.doesCafeExist(cafe, function(booler){
+      if(booler){
+        res.status(400).send('That cafe already exists.');
+      }
+      else{
+        db.addCafe(cafe, function(){
+          db.getCafe(cafe, function(cafeObj){
+            res.status(200).send(JSON.stringify(cafeObj));
+          });
+        });
+      }
+    });
   }
 };
