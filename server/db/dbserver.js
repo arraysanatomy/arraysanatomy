39907bbdb71ca11d935c6974abe72040b4333329
MKS-Student <mks.student@hackreactor.com>
2016-04-08 18:43:50 -0700

    bug: button fixed / remove margin main page

--- server/db/dbserver.js
+++ server/db/dbserver.js
@@ -155,11 +155,9 @@ function sqliteEscape (str) {
             case "'":
                 return "\'";
             case "\"":
-            // case "'":
             case "\\":
             case "%":
-                return "\\"+char; // prepends a backslash to backslash, percent,
-                                  // and double/single quotes
+                return "\\"+char; // prepends a backslash to backslash and, percent.
         }
     });
 }
